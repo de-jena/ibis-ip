9250d38a1eb35f3fbf9c0677eff10449c1685956
Guido Grune <g.grune@datainmotion.com>
2024-07-26 15:54:18 +0200

    remove event handling, publish direct to mqtt
    Signed-off-by: Guido Grune <g.grune@datainmotion.com>

--- de.jena.ibis.rest.application/src/de/jena/ibis/rest/application/resource/IbisResource.java
+++ de.jena.ibis.rest.application/src/de/jena/ibis/rest/application/resource/IbisResource.java
@@ -11,23 +11,30 @@
  */
 package de.jena.ibis.rest.application.resource;
 
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.gecko.emf.json.annotation.RequireEMFJson;
+import org.gecko.emf.json.constants.EMFJs;
+import org.gecko.osgi.messaging.MessagingService;
 import org.osgi.service.component.ComponentServiceObjects;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventAdmin;
 import org.osgi.service.jakartars.whiteboard.propertytypes.JakartarsResource;
 
 import de.jena.ibis.apis.helper.IbisResponseHelper;
@@ -47,17 +54,20 @@ import jakarta.ws.rs.core.Response;
  * @since Mar 29, 2023
  */
 @RequireRuntime
+@RequireEMFJson
 @JakartarsResource
-@Component(name = IbisResource.COMPONENT_NAME, service = IbisResource.class, 
-scope = ServiceScope.PROTOTYPE, configurationPolicy = ConfigurationPolicy.REQUIRE)
+@Component(name = IbisResource.COMPONENT_NAME, service = IbisResource.class, scope = ServiceScope.PROTOTYPE, configurationPolicy = ConfigurationPolicy.REQUIRE)
 @Path("")
 public class IbisResource {
+	private static final Logger LOGGER = Logger.getLogger(IbisResource.class.getName());
+
+	private static final String TOPIC = "5g/ibis/";
+
+	@Reference(target = "(id=full)")
+	private MessagingService messaging;
 
 	@Reference
-	EventAdmin eventAdmin;
-
-	@Reference
-	private ComponentServiceObjects<ResourceSet> rsFactory;
+	private ComponentServiceObjects<ResourceSet> serviceObject;
 
 	public static final String COMPONENT_NAME = "IbisJakartarsResource";
 
@@ -68,46 +78,64 @@ public class IbisResource {
 	}
 
 	@POST
-	@Path("/{deviceId}/{deviceType}/{serviceName}/{operationName}")
+	@Path("/{deviceType}/{deviceId}/{serviceName}/{operationName}")
 	@Consumes
-	public Response post(@PathParam("deviceId") String deviceId, @PathParam("deviceType") String deviceType, @PathParam("serviceName") String serviceName, @PathParam("operationName") String operationName, @Context HttpServletRequest request) {
+	public Response post(@PathParam("deviceType") String deviceType, @PathParam("deviceId") String deviceId,
+			@PathParam("serviceName") String serviceName, @PathParam("operationName") String operationName,
+			@Context HttpServletRequest request) {
 
-		System.out.println(String.format("Received POST request to %s/%s/%s/%s", deviceId, deviceType, serviceName, operationName));
-		if(request != null) {
-			try {
-
-				EClass responseEClass = IbisResponseHelper.getResponseEClass(serviceName, operationName);
-				if(responseEClass != null) {
-					ResourceSet set = rsFactory.getService();
+		LOGGER.info(String.format("Received POST request to %s/%s/%s/%s", deviceType, deviceId, serviceName,
+				operationName));
+		if (request != null) {
+			EClass responseEClass = IbisResponseHelper.getResponseEClass(serviceName, operationName);
+			if (responseEClass != null) {
+				ResourceSet set = serviceObject.getService();
+				try {
 					set.getPackageRegistry().put(null, responseEClass.getEPackage());
-					Resource responseRes = set.createResource(URI.createURI("temp.xml"), "application/xml");
-					Map<String, Object> responseOptions = new HashMap<>();
-					responseOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
-					responseOptions.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
-					responseOptions.put(XMLResource.OPTION_ENCODING, "UTF-8");
-					responseOptions.put(XMLResource.OPTION_KEEP_DEFAULT_CONTENT, true);
-					responseRes.load(request.getInputStream(), responseOptions);
-					Map<String, Object> properties = new HashMap<>();
-					properties.put("deviceId", deviceId);
-					properties.put("deviceType", deviceType);
-					properties.put("serviceName", serviceName);
-					properties.put("operation", operationName);
-					properties.put("eclass", responseEClass);
-					properties.put("data", responseRes.getContents().get(0));
-
-					Event evt = new Event(String.format("TCPResponse/%s/%s/%s/%s", deviceId, deviceType, serviceName, operationName), properties);								
-					eventAdmin.postEvent(evt);
-					System.out.println("Resource loaded successfully!");
+					Optional<EObject> data = load(request, set);
+					if (data.isPresent()) {
+						String topic = TOPIC + deviceType + "/" + deviceId + "/" + serviceName + "/" + operationName;
+						publish(set, topic, data.get());
+					}
+				} finally {
+					serviceObject.ungetService(set);
 				}
-
-			} catch(IOException e) {
-				e.printStackTrace();
 			}
-		}
-		else {
-			System.out.println("Request is null!");
 
+			LOGGER.finer("Resource loaded successfully!");
+		} else {
+			LOGGER.finer("Request is null!");
 		}
 		return Response.ok().build();
+	}
+
+	private Optional<EObject> load(HttpServletRequest request, ResourceSet set) {
+		try {
+			Resource responseRes = set.createResource(URI.createURI(UUID.randomUUID().toString() + "-load.xml"),
+					"application/xml");
+			Map<String, Object> responseOptions = new HashMap<>();
+			responseOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+			responseOptions.put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
+			responseOptions.put(XMLResource.OPTION_ENCODING, "UTF-8");
+			responseOptions.put(XMLResource.OPTION_KEEP_DEFAULT_CONTENT, true);
+			responseRes.load(request.getInputStream(), responseOptions);
+			return Optional.of(responseRes.getContents().get(0));
+		} catch (IOException e) {
+			LOGGER.severe("Error while loading ibis resource." + e.getLocalizedMessage());
+			e.printStackTrace();
+			return Optional.empty();
+		}
+	}
+
+	private void publish(ResourceSet set, String topic, EObject data) {
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			Resource resource = set.createResource(URI.createFileURI(UUID.randomUUID().toString() + "-mqtt.json"));
+			resource.getContents().add(data);
+			resource.save(baos, Collections.singletonMap(EMFJs.OPTION_SERIALIZE_DEFAULT_VALUE, true));
+			messaging.publish(topic, ByteBuffer.wrap(baos.toByteArray()));
+		} catch (Exception e) {
+			LOGGER.severe("Error while publishing ibis resource." + e.getLocalizedMessage());
+			e.printStackTrace();
+		}
 	}
 }
